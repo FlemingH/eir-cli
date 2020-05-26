ec7ca498b8758fd180e563aea0ce514fe88ba8ae
flm <lm.fei@outlook.com>
2020-05-26 22:19:40 +0800

    shou project init

--- bin/init_v1/init_element.js
+++ bin/init_v1/init_element.js
@@ -2,8 +2,16 @@ var copy_file = require("../util/copy_file");
 var path = require('path')
 
 function createByElementui () {
-    var from = path.join(__dirname, '../../templates/templates_v1/templates_elementui');
-    copy_file.copyFolder(from, '.');
+    console.log("init project...");
+    try {
+        var from = path.join(__dirname, '../../templates/templates_v1/templates_elementui');
+        copy_file.copyFolder(from, '.');
+    } catch (e) {
+        console.log('init fail');
+    }
+    console.log("init complete!\n");
+    console.log('① run\tnpm init\n');
+    console.log('② run\tnpm install\n')
 }
 
 module.exports = {
