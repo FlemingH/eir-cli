24ed09617d187ced9b4e6b36ff2bd325ec7a0ae1
FlemingH <lm.fei@outlook.com>
2020-07-22 11:10:52 +0800

    get page data

--- templates/templates_v1/templates_elementui/src/controller/eir_root.js
+++ templates/templates_v1/templates_elementui/src/controller/eir_root.js
@@ -1,3 +1,4 @@
+import InDB from 'indb'
 
 export default {
     name: 'EirRoot',
@@ -7,12 +8,26 @@ export default {
     },
     data: function() {
         return {
+            eirDb: null,
+            eirUser: null,
+
+            curActivatedUserPage: [],
         }
     },
     watch: {
     },
     methods: {
+        init() {
+            this.eirDb = new InDB(this.global.db_options);
+            this.eirUser = this.eirDb.use("eir_user");
+            this.initSideBarData();
+        },
+        async initSideBarData() {
+            var curActivatedUser = await this.eirUser.find('activited', "1");
+            this.curActivatedUserPage = JSON.parse(curActivatedUser["page"]);
+        },
     },
     mounted: function() {
+        this.init();
     }
 }
